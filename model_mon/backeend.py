6411b1db5c93ff1c25d8985da29f25e66119ce38
rioanalytical <115632638+rioanalytical@users.noreply.github.com>
2025-08-21 15:11:18 +0530

    Update backeend.py

--- model_mon/backeend.py
+++ model_mon/backeend.py
@@ -2,19 +2,16 @@ import pandas as pd
 from sklearn import datasets
 from evidently import Report
 from evidently.presets import DataDriftPreset
-from flask import request, jsonify
+from flask import Response
 
-@app.route("/run_eval", methods=["POST"])
-def run_eval():
+@app.route("/drift_html", methods=["GET"])
+def drift_html():
     iris_data = datasets.load_iris(as_frame=True)
     iris_frame = iris_data.frame
-
-    # Split current and reference data
     current_data = iris_frame.iloc[:60]
     reference_data = iris_frame.iloc[60:]
-    
-    # Run Evidently Data Drift preset with PSI
     report = Report([DataDriftPreset(method="psi")], include_tests=True)
-    result = report.run(current_data, reference_data)
-    # Return Evaluation as JSON
-    return jsonify(result.as_dict())
+    report.run(current_data, reference_data)
+    # Get HTML as string (using internal method, since save_html saves to file)
+    html = report.get_html()
+    return Response(html, mimetype="text/html")
