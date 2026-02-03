8bf4f33ae30412ef5e6bd9ccbb4bd5d4c6c65ecf
rioanalytical <115632638+rioanalytical@users.noreply.github.com>
2026-02-03 15:00:20 +0530

    Delete project_mind directory

--- project_mind/env_example.sh
+++
@@ -1,6 +0,0 @@
-OPENAI_API_KEY=your_openai_api_key_here
-MOCK_API_HOST=localhost
-MOCK_API_PORT=8000
-LLM_MODEL=gpt-4-turbo-preview
-EMBEDDING_MODEL=text-embedding-3-small
-TEMPERATURE=0.7
--- project_mind/run_mock_server.py
+++
@@ -1,5 +0,0 @@
-"""Script to run the mock API server"""
-from mock_apis import start_server
-
-if __name__ == "__main__":
-    start_server(host="0.0.0.0", port=8000)
